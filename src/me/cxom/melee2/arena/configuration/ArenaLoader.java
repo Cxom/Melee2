4ce958cda5c8ddfe6ee5ce2c5e36e79bdd23811b
Cxom <jeremybk@sbcglobal.net>
2017-02-11 20:07:45 -0500

    Add ArenaLoader, ArenaManager classes

---
+++ src/me/cxom/melee2/arena/configuration/ArenaLoader.java
@@ -0,0 +1,85 @@
+package me.cxom.melee2.arena.configuration;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import me.cxom.melee2.arena.MeleeArena;
+
+public class ArenaLoader {
+	
+	public static MeleeArena load(FileConfiguration arenacfg){
+		String name = arenacfg.getString("name");
+		
+		World world = getRootWorld(arenacfg);
+		
+		List<Location> spawns = getList(arenacfg.getConfigurationSection("spawns"),
+										(ConfigurationSection section) -> { return getLocation(section, world); });
+		
+		Location pregameLobby;
+		if (arenacfg.isConfigurationSection("lobby")){
+			pregameLobby = getLocation(arenacfg.getConfigurationSection("lobby"));
+		} else {
+			pregameLobby = spawns.get(0);
+		}
+		
+		int playersToStart = arenacfg.getInt("playersToStart", 2);
+		int killsToEnd = arenacfg.getInt("killsToEnd", 7);
+		
+		return new MeleeArena(name, pregameLobby, playersToStart, spawns, killsToEnd);
+	};
+	
+	/*Static loading parsers*/
+	
+	public static World getWorld(ConfigurationSection section){
+		return Bukkit.getWorld(section.getString("world"));
+	}
+	
+	public static World getRootWorld(ConfigurationSection section){
+		return getWorld(section.getRoot());
+	}
+	
+	public static String getName(File arenaFile){
+		String name = arenaFile.getName();
+        return name.substring(0, name.length() - 4);
+	}
+	
+	public static <T> List<T> getList(ConfigurationSection section, Function<ConfigurationSection, T> loader) throws ClassCastException{
+		List<T> list = new ArrayList<>();
+		for(String key : section.getKeys(false)){
+			try {
+				list.add(loader.apply(section.getConfigurationSection(key)));
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	public static Location getLocation(ConfigurationSection spawnInfo){
+		return getLocation(spawnInfo, getRootWorld(spawnInfo));
+	}
+	
+	public static Location getLocationWithWorld(ConfigurationSection section){
+		return getLocation(section, getWorld(section));
+	}
+	
+	public static Location getLocation(ConfigurationSection spawnInfo, World world){
+		return new Location(
+			world,
+			spawnInfo.getDouble("x"),
+			spawnInfo.getDouble("y"),
+			spawnInfo.getDouble("z"),
+			(float) spawnInfo.getDouble("pitch", 0),
+			(float) spawnInfo.getDouble("yaw", 0) //TODO Is 0 pitch, 0 yaw looking straight forward?
+		);
+	}
+	
+}
---
+++ src/me/cxom/melee2/arena/configuration/ArenaManager.java
@@ -0,0 +1,130 @@
+package me.cxom.melee2.arena.configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import me.cxom.melee2.Melee;
+import me.cxom.melee2.arena.MeleeArena;
+
+public class ArenaManager {
+	
+	private static File arenaFolder;
+	
+	private static Map<String, FileConfiguration> arenaConfigs = new HashMap<>();
+	
+	private static Map<String, MeleeArena> arenas = new HashMap<>();
+	
+	public static Collection<MeleeArena> getArenas(){
+		return arenas.values();
+	}
+	
+	public static boolean isArena(String arenaName){
+		return arenas.containsKey(arenas) || loadArenaConfig(arenaName) != null;
+	}
+	
+	public static MeleeArena getArena(String arenaName){
+		if (!isArena(arenaName)) return null;
+		return arenas.get(arenaName);
+	}
+	
+	public static FileConfiguration getArenaConfig(String arenaName){
+		if (!isArena(arenaName)) return null;
+		return arenaConfigs.get(arenaName);
+	}
+	
+	public static void modifyArena(String arenaName, String path, Object value){
+		if(!isArena(arenaName)) return;
+		FileConfiguration arena = getArenaConfig(arenaName);
+		arena.set(path, value);
+		arenaConfigs.put(arenaName, arena);
+		MeleeArena loaded = ArenaLoader.load(arena);
+		if (loaded != null){
+			arenas.put(arenaName, loaded);
+		}
+	}
+	
+	public static void loadArenas(){
+		
+		arenaFolder = new File(Melee.getPlugin().getDataFolder().getAbsolutePath()
+				+ File.separator + "Arenas");
+		
+		
+		if(!arenaFolder.exists()){
+			arenaFolder.mkdirs();
+		}
+		
+		if(arenaFolder.listFiles() != null){
+			for (File arenaf : Arrays.asList(arenaFolder.listFiles())) {
+				FileConfiguration arena = new YamlConfiguration();
+				try {
+					arena.load(arenaf);
+					arenaConfigs.put(arenaf.getName(), arena);
+					MeleeArena loaded = ArenaLoader.load(arena);
+					if (loaded != null){
+						arenas.put(arenaf.getName(), loaded);
+					}
+				} catch (IOException | InvalidConfigurationException e) {
+					Bukkit.getLogger().warning("Could not load " + arenaf.getName() + "!");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static FileConfiguration loadArenaConfig(String arenaName){
+		File arenaf = new File(Melee.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "arenas" + File.separator + arenaName);
+		if(!arenaf.exists()){
+			return null;
+		} else {
+			FileConfiguration arena = new YamlConfiguration();
+			try {
+				arena.load(arenaf);
+				arenaConfigs.put(arenaName, arena);
+				MeleeArena loaded = ArenaLoader.load(arena);
+				if (loaded != null){
+					arenas.put(arenaf.getName(), loaded);
+				}
+				return arena;
+			} catch (IOException | InvalidConfigurationException e) {
+				Bukkit.getLogger().warning("Could not load " + arenaf.getName() + "!");
+				e.printStackTrace();
+				return null;
+			}
+		}
+		
+	}
+	
+	public static void save(String arenaName){
+		if (!isArena(arenaName)) throw new IllegalArgumentException();
+		File arenaf = new File(Melee.getPlugin().getDataFolder().getAbsolutePath()
+				+ File.separator + "Arenas" + File.separator + arenaName + ".yml");
+		try {
+			getArenaConfig(arenaName).save(arenaf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveAll(){
+		for(Map.Entry<String, FileConfiguration> arenaEntry : arenaConfigs.entrySet()){
+			File arenaf = new File(Melee.getPlugin().getDataFolder().getAbsolutePath()
+					+ File.separator + "Arenas" + File.separator + arenaEntry.getKey() + ".yml");
+			try {
+				arenaEntry.getValue().save(arenaf);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+}
